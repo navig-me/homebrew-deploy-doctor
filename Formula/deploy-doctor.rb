ada77a899f92ddcb19fd39de493e7ab2641404c4
Mihir Khandekar <mihirkhandekar@gmail.com>
2026-05-29 14:56:22 +0800

    Update deploy-doctor formula checksums for v0.1.0

--- Formula/deploy-doctor.rb
+++ Formula/deploy-doctor.rb
@@ -6,10 +6,10 @@ class DeployDoctor < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/navig-me/deploy-doctor/releases/download/v0.1.0/deploy-doctor_0.1.0_darwin_arm64.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "446244f63fd52bf641555301ff1e0215d3ae3e31ed502ada223b9e40a292387c"
     else
       url "https://github.com/navig-me/deploy-doctor/releases/download/v0.1.0/deploy-doctor_0.1.0_darwin_amd64.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "db092814e925f6c2dacffeb1f51be771e7739ba90020a92d36fe30f0e357ce3a"
     end
   end
 
